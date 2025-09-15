382b6ac07055cbbe6ab11a5544290e9bf4dd25ed
e.olenin <e.olenin@expert-it.su>
2025-09-15 06:03:49 +0300

    stats

--- ewm-stats/src/main/java/ru/practicum/ewm/stats/repository/StatsRepository.java
+++ ewm-stats/src/main/java/ru/practicum/ewm/stats/repository/StatsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.practicum.ewm.stats.dto.ViewStats;
 import ru.practicum.ewm.stats.model.EndpointHitEntityModel;
 
 import java.time.LocalDateTime;
@@ -18,9 +19,9 @@ public interface StatsRepository extends JpaRepository<EndpointHitEntityModel, L
            "AND (:uris IS NULL OR e.uri IN :uris) " +
            "GROUP BY e.app, e.uri " +
            "ORDER BY COUNT(e) DESC")
-    List<ru.practicum.ewm.stats.dto.ViewStats> getStats(@Param("start") LocalDateTime start,
-                                                         @Param("end") LocalDateTime end,
-                                                         @Param("uris") List<String> uris);
+    List<ViewStats> getStats(@Param("start") LocalDateTime start,
+                             @Param("end") LocalDateTime end,
+                             @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.ewm.stats.dto.ViewStats(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
            "FROM EndpointHitEntityModel e " +
@@ -28,7 +29,7 @@ public interface StatsRepository extends JpaRepository<EndpointHitEntityModel, L
            "AND (:uris IS NULL OR e.uri IN :uris) " +
            "GROUP BY e.app, e.uri " +
            "ORDER BY COUNT(DISTINCT e.ip) DESC")
-    List<ru.practicum.ewm.stats.dto.ViewStats> getUniqueStats(@Param("start") LocalDateTime start,
-                                                              @Param("end") LocalDateTime end,
-                                                              @Param("uris") List<String> uris);
+    List<ViewStats> getUniqueStats(@Param("start") LocalDateTime start,
+                                   @Param("end") LocalDateTime end,
+                                   @Param("uris") List<String> uris);
 }
